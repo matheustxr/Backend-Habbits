b156100c9ab6bd6b36b51d9f9517410d6e428a51
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-12-27 16:27:01 -0300

    finished created endpoint UpdateUser

--- src/Habbits.Application/UseCases/Users/Update/UpdateUserUseCase.cs
+++ src/Habbits.Application/UseCases/Users/Update/UpdateUserUseCase.cs
@@ -1,5 +1,66 @@
-﻿namespace Habbits.Application.UseCases.Users.Update;
+﻿using Habbits.Communication.Requests;
+using Habbits.Domain.Repositories.User;
+using Habbits.Domain.Repositories;
+using Habbits.Domain.Services.LoggedUser;
+using FluentValidation.Results;
+using Habbits.Exception.ExceptionBase;
+using Habbits.Exception;
 
-public class UpdateUserUseCase
+namespace Habbits.Application.UseCases.Users.Update;
+
+public class UpdateUserUseCase : IUpdateUserUseCase
 {
+    private readonly ILoggedUser _loggedUser;
+    private readonly IUserUpdateOnlyRepository _repository;
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    private readonly IUnityOfWork _unitOfWork;
+
+    public UpdateUserUseCase(
+        ILoggedUser loggedUser,
+        IUserUpdateOnlyRepository repository,
+        IUserReadOnlyRepository userReadOnlyRepository,
+        IUnityOfWork unityOfWork)
+    {
+        _loggedUser = loggedUser;
+        _repository = repository;
+        _unitOfWork = unityOfWork;
+        _userReadOnlyRepository = userReadOnlyRepository;
+    }
+
+    public async Task Execute(RequestUpdateUserJson request)
+    {
+        var loggedUser = await _loggedUser.Get();
+
+        await Validate(request, loggedUser.Email);
+
+        var user = await _repository.GetById(loggedUser.Id);
+
+        user.Name = request.Name;
+        user.Email = request.Email;
+
+        _repository.Update(user);
+
+        await _unitOfWork.Commit();
+    }
+
+    private async Task Validate(RequestUpdateUserJson request, string currentEmail)
+    {
+        var validator = new UpdateUserValidator();
+
+        var result = validator.Validate(request);
+
+        if (currentEmail.Equals(request.Email) == false)
+        {
+            var userExist = await _userReadOnlyRepository.ExistActiveUserWithEmail(request.Email);
+            if (userExist)
+                result.Errors.Add(new ValidationFailure(string.Empty, ResourceErrorMessages.EMAIL_ALREADY_REGISTERED));
+        }
+
+        if (result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
 }
---
+++ src/Habbits.Application/UseCases/Users/Update/UpdateUserValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Habbits.Communication.Requests;
+using Habbits.Exception;
+
+namespace Habbits.Application.UseCases.Users.Update;
+
+public class UpdateUserValidator : AbstractValidator<RequestUpdateUserJson>
+{
+    public UpdateUserValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
+        RuleFor(user => user.Email)
+            .NotEmpty()
+            .WithMessage(ResourceErrorMessages.EMAIL_EMPTY)
+            .EmailAddress()
+            .When(user => string.IsNullOrWhiteSpace(user.Email) == false, ApplyConditionTo.CurrentValidator)
+            .WithMessage(ResourceErrorMessages.EMAIL_INVALID);
+    }
+}
