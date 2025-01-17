1a595aced81ab1c50a557b94fe8fc519742bde04
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-01-17 11:52:08 -0300

    created entity habit

--- src/Habbits.Domain/Entities/User.cs
+++ src/Habbits.Domain/Entities/User.cs
@@ -3,8 +3,14 @@
 public class User
 {
     public long Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public Guid UserIdentifier { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public List<int> WeekDays { get; set; } = new();
+
+    public bool IsActive { get; set; } = true;
 }
