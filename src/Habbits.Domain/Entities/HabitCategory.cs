738b5621c8752a6f0850a67854497606a39ca0cb
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-01-22 14:43:43 -0300

    create HabitCategory entity to organize habits

---
+++ src/Habbits.Domain/Entities/HabitCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace Habbits.Domain.Entities;
+
+public class HabitCategory
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public List<Habit> Habits { get; set; } = [];
+}
