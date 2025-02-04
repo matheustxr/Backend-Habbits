873f5ae74d11e7bd62748788159c14edd709e253
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-04 20:37:08 -0300

    created response for habit complete

---
+++ src/Habbits.Communication/Responses/Habbits/ResponseFullHabit.cs
@@ -0,0 +1,12 @@
+﻿using Habbits.Communication.Enums;
+
+namespace Habbits.Communication.Responses.Habbits
+{
+    public class ResponseFullHabit
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public List<WeekDays> WeekDays { get; set; } = new();
+        public bool IsActive { get; set; }
+    }
+}
