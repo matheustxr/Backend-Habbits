8d8b6f00d3dc2e89e21619a245a69b115275d292
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-12 11:33:55 -0300

    created DayHabitBuilder

---
+++ tests/CommonTestUtilities/Entities/DayHabitBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Habbits.Domain.Entities;
+
+namespace CommonTestUtilities.Entities
+{
+    public class DayHabitBuilder
+    {
+        public static DayHabit Build(long habitId)
+        {
+            return new Faker<DayHabit>()
+                .RuleFor(d => d.Id, _ => 1)
+                .RuleFor(d => d.Date, faker => faker.Date.Past())
+                .RuleFor(d => d.IsCompleted, faker => faker.Random.Bool())
+                .RuleFor(d => d.HabitId, _ => habitId);
+        }
+    }
+}
