4c5cb585715f6be44696d3a506054d7b997dd627
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-12 11:29:46 -0300

    created UserBuilder

---
+++ tests/CommonTestUtilities/Cryptography/PasswordEncripterBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Habbits.Domain.Security.Cryptography;
+using Moq;
+
+namespace CommonTestUtilities.Cryptography
+{
+    public class PasswordEncripterBuilder
+    {
+        private readonly Mock<IPasswordEncripter> _mock;
+
+        public PasswordEncripterBuilder()
+        {
+            _mock = new Mock<IPasswordEncripter>();
+
+            _mock.Setup(passwordEncrypter => passwordEncrypter.Encrypt(It.IsAny<string>())).Returns("!% dlfjkd545");
+        }
+
+        public PasswordEncripterBuilder Verify(string? password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                _mock.Setup(passwordEncrypter => passwordEncrypter.Verify(password, It.IsAny<string>())).Returns(true);
+            }
+
+            return this;
+        }
+
+        public IPasswordEncripter Build() => _mock.Object;
+    }
+}
