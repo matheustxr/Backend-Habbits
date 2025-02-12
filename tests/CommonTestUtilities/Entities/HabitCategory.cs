a6965a37b7864f92cd3c2a11be76dfb320a5aa6d
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-12 11:35:02 -0300

    created HabitBuilder

---
+++ tests/CommonTestUtilities/Entities/HabitCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using Habbits.Domain.Entities;
+
+namespace CommonTestUtilities.Entities
+{
+    public class HabitCategoryBuilder
+    {
+        public static HabitCategory Build()
+        {
+            return new Faker<HabitCategory>()
+                .RuleFor(c => c.Id, _ => 1)
+                .RuleFor(c => c.Category, faker => faker.Commerce.Department());
+        }
+    }
+}
