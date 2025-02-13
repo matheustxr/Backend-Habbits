aeef5b1a40c2c9bd0a3889a86ae665f525856d22
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-13 14:19:55 -0300

    created UserUpdateOnlyRepositoryBuilder

---
+++ tests/CommonTestUtilities/Repositories/Users/UserUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Habbits.Domain.Entities;
+using Habbits.Domain.Repositories.User;
+using Moq;
+
+namespace CommonTestUtilities.Repositories.Users
+{
+    public class UserUpdateOnlyRepositoryBuilder
+    {
+        public static IUserUpdateOnlyRepository Build(User user)
+        {
+            var mock = new Mock<IUserUpdateOnlyRepository>();
+
+            mock.Setup(repositoy => repositoy.GetById(user.Id)).ReturnsAsync(user);
+
+            return mock.Object;
+        }
+    }
+}
