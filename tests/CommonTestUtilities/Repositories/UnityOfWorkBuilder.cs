a45b03bc873bc02781d0a16297f2c1e26ffee055
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-13 12:26:17 -0300

    created UnityOfWorkBuilder

---
+++ tests/CommonTestUtilities/Repositories/UnityOfWorkBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Habbits.Domain.Repositories;
+using Moq;
+
+namespace CommonTestUtilities.Repositories
+{
+    public class UnityOfWorkBuilder
+    {
+        public static IUnityOfWork Build()
+        {
+            var mock = new Mock<IUnityOfWork>();
+
+            return mock.Object;
+        }
+    }
+}
