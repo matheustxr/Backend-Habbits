40dc8e5436e92c03ebacbcc55a78e536baf86e38
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-15 21:52:39 -0300

    created IUsertWriteOnlyRepository

--- tests/CommonTestUtilities/Repositories/Users/UserWriteOnlyRepositoryBuilder.cs
+++ tests/CommonTestUtilities/Repositories/Users/UserWriteOnlyRepositoryBuilder.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Habbits.Domain.Repositories.User;
-using Moq;
+﻿using Moq;
 
 namespace CommonTestUtilities.Repositories.Users
 {
-    public class UserWriteOnlyRepositoryBuilder
+    public class IUsertWriteOnlyRepository
     {
         public static IUsertWriteOnlyRepository Build()
         {
