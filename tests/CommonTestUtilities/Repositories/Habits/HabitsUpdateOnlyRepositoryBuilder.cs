496a1762e168efa25a609e0ff94205a003a9bbf2
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-16 09:48:25 -0300

    created HabitsUpdateOnlyRepositoryBuilder

---
+++ tests/CommonTestUtilities/Repositories/Habits/HabitsUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Habbits.Domain.Repositories.Habit;
+using Moq;
+
+namespace CommonTestUtilities.Repositories.Habits
+{
+    public class HabitsUpdateOnlyRepositoryBuilder
+    {
+        public static IHabitUpdateOnlyRepository Build()
+        {
+            var mock = new Mock<IHabitUpdateOnlyRepository>();
+
+            return mock.Object;
+        }
+    }
+}
