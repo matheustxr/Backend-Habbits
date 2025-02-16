2552d0c3a81123db26d6fdf29d3ffa0721e74514
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-16 09:45:02 -0300

    created IHabitReadOnlyRepository

---
+++ tests/CommonTestUtilities/Repositories/Habits/HabitsReadOnlyRepository.cs
@@ -0,0 +1,15 @@
+﻿using Habbits.Domain.Repositories.Habit;
+using Moq;
+
+namespace CommonTestUtilities.Repositories.Habits
+{
+    public class HabitsReadOnlyRepository
+    {
+        public static IHabitReadOnlyRepository Build()
+        {
+            var mock = new Mock<IHabitReadOnlyRepository>();
+
+            return mock.Object;
+        }
+    }
+}
