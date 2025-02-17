f4df6e4b8a9c591169a6fd0298819d8b691f800e
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-17 13:51:32 -0300

    RequestRegisterUserJsonBuilder

---
+++ tests/CommonTestUtilities/Requests/User/RequestRegisterUserJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using Habbits.Communication.Requests.User;
+
+namespace CommonTestUtilities.Requests.User
+{
+    public class RequestRegisterUserJsonBuilder
+    {
+        public static RequestRegisterUserJson Build()
+        {
+            return new Faker<RequestRegisterUserJson>()
+                .RuleFor(user => user.Name, faker => faker.Person.FirstName)
+                .RuleFor(user => user.Email, (faker, user) => faker.Internet.Email(user.Name))
+                .RuleFor(user => user.Password, faker => faker.Internet.Password(prefix: "!Aa1"));
+        }
+    }
+}
