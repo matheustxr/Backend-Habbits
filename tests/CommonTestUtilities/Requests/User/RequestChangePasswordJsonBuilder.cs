da38225cc953615a87d7d3170ee1779b3f135aaa
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-17 13:44:46 -0300

    created RequestChangePasswordJsonBuilder

---
+++ tests/CommonTestUtilities/Requests/User/RequestChangePasswordJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using Habbits.Communication.Requests.User;
+
+namespace CommonTestUtilities.Requests.User
+{
+    public class RequestChangePasswordJsonBuilder
+    {
+        public static RequestChangePasswordJson Build()
+        {
+            return new Faker<RequestChangePasswordJson>()
+                .RuleFor(user => user.Password, faker => faker.Internet.Password())
+                .RuleFor(user => user.NewPassword, faker => faker.Internet.Password(prefix: "!Aa1"));
+        }
+    }
+}
