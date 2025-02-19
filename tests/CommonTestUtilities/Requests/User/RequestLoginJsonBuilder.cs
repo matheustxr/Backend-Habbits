e26bfe93efa951694061ffaf24efc2aacc910490
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-02-18 21:16:51 -0300

    created RequestLoginJsonBuilder

---
+++ tests/CommonTestUtilities/Requests/User/RequestLoginJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using Habbits.Communication.Requests.User;
+
+namespace CommonTestUtilities.Requests.User
+{
+    public class RequestLoginJsonBuilder
+    {
+        public static RequestLoginJson Build()
+        {
+            return new Faker<RequestLoginJson>()
+                .RuleFor(user => user.Email, faker => faker.Internet.Email())
+                .RuleFor(user => user.Password, faker => faker.Internet.Password(prefix: "!Aa1"));
+        }
+    }
+}
