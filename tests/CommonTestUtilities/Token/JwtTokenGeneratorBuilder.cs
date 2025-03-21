622cce97677959ccec0bd2b1e374bcd0335ec54a
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-03-21 14:16:56 -0300

    created JwtTokenGeneratorBuilder

--- tests/CommonTestUtilities/Token/JwtTokenGeneratorBuilder.cs
+++ tests/CommonTestUtilities/Token/JwtTokenGeneratorBuilder.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Habbits.Domain.Entities;
+using Habbits.Domain.Security.Tokens;
+using Moq;
 
 namespace CommonTestUtilities.Token
 {
-    internal class JwtTokenGeneratorBuilder
+    public class JwtTokenGeneratorBuilder
     {
+        public static IAccessTokenGenerator Build()
+        {
+            var mock = new Mock<IAccessTokenGenerator>();
+
+            mock.Setup(accessTokenGenerator => accessTokenGenerator.Generate(It.IsAny<User>()))
+                .Returns("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+
+            return mock.Object;
+        }
     }
 }
