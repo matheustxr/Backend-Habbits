5072f16eee8ff94e82a64d6da3e39a0cf116000e
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-04-05 11:19:08 -0300

    created DoLoginUseCaseTest

--- tests/UseCases/Login/DoLoginUseCaseTest.cs
+++ tests/UseCases/Login/DoLoginUseCaseTest.cs
@@ -1,12 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories.Users;
+using CommonTestUtilities.Requests.User;
+using CommonTestUtilities.Token;
+using FluentAssertions;
+using Habbits.Application.UseCases.Login;
+using Habbits.Exception.ExceptionBase;
+using Habbits.Exception;
 
 namespace UseCases.Test.Login
 {
-    internal class DoLoginUseCaseTest
+    public class DoLoginUseCaseTest
     {
+        [Fact]
+        public async Task Success()
+        {
+            var user = UserBuilder.Build();
+
+            var request = RequestLoginJsonBuilder.Build();
+            request.Email = user.Email;
+
+            var useCase = CreateUseCase(user, request.Password);
+
+            var result = await useCase.Execute(request);
+
+            result.Should().NotBeNull();
+            result.Name.Should().Be(user.Name);
+            result.Token.Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public async Task Error_User_Not_Found()
+        {
+            var user = UserBuilder.Build();
+
+            var request = RequestLoginJsonBuilder.Build();
+
+            var useCase = CreateUseCase(user, request.Password);
+
+            var act = async () => await useCase.Execute(request);
+
+            var result = await act.Should().ThrowAsync<InvalidLoginException>();
+
+            result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID));
+        }
+
+        [Fact]
+        public async Task Error_Password_Not_Match()
+        {
+            var user = UserBuilder.Build();
+
+            var request = RequestLoginJsonBuilder.Build();
+            request.Email = user.Email;
+
+            var useCase = CreateUseCase(user);
+
+            var act = async () => await useCase.Execute(request);
+
+            var result = await act.Should().ThrowAsync<InvalidLoginException>();
+
+            result.Where(ex => ex.GetErrors().Count == 1 && ex.GetErrors().Contains(ResourceErrorMessages.EMAIL_OR_PASSWORD_INVALID));
+        }
+
+        private DoLoginUseCase CreateUseCase(Habbits.Domain.Entities.User user, string? password = null)
+        {
+            var passwordEncripter = new PasswordEncrypterBuilder().Verify(password).Build();
+
+            var tokenGenerator = JwtTokenGeneratorBuilder.Build();
+
+            var readRepository = new UserReadOnlyRepositoryBuilder().GetUserByEmail(user).Build();
+
+            return new DoLoginUseCase(readRepository, passwordEncripter, tokenGenerator);
+        }
     }
 }
