4e8133a3c5442ea848886e11d9573e368631a841
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-04-10 14:12:57 -0300

    created HabitIdentityManager in webApiTest

--- tests/WebApi.Test/Resources/HabitIdentityManager.cs
+++ tests/WebApi.Test/Resources/HabitIdentityManager.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Habbits.Domain.Entities;
 
 namespace WebApi.Test.Resources
 {
-    internal class HabitIdentityManager
+    public class HabitIdentityManager
     {
+        private readonly Habit _habit;
+
+        public HabitIdentityManager(Habit habit)
+        {
+            _habit = habit;
+        }
+
+        public long GetId() => _habit.Id;
+        public string GetTitle() => _habit.Title;
+        public DateTime GetCreatedAt() => _habit.CreatedAt;
+        public Guid GetUserId() => _habit.UserId;
+        public bool IsActive() => _habit.IsActive;
+        public long? GetCategoryId() => _habit.CategoryId;
     }
 }
