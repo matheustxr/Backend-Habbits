f2ce0fdebb6253928033c98e32441dec911f692c
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-04-23 19:58:05 -0300

    update ResponseHabitJson and created ResponseShortHabitJson

--- src/Habits.Communication/Responses/Habits/ResponseHabitJson.cs
+++ src/Habits.Communication/Responses/Habits/ResponseHabitJson.cs
@@ -2,11 +2,16 @@
 
 namespace Habits.Communication.Responses.Habits
 {
-    public class ResponseFullHabit
+    public class ResponseHabitJson
     {
+        public long Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
         public List<WeekDays> WeekDays { get; set; } = new();
         public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public long? CategoryId { get; set; }
+        public string? CategoryName { get; set; }
     }
 }
---
+++ src/Habits.Communication/Responses/Habits/ResponseShortHabitJson.cs
@@ -0,0 +1,14 @@
+﻿using Habits.Communication.Enums;
+
+namespace Habits.Communication.Responses.Habits
+{
+    public class ResponseShortHabitJson
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public List<WeekDays> WeekDays { get; set; } = new();
+        public bool IsActive { get; set; }
+        public long? CategoryId { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
