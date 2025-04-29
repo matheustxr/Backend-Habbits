534aaf5fc7cb8685850601f4197b7b0551bc1171
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-04-29 20:18:38 -0300

    created GetAllHabitsTest

---
+++ tests/WebApi.Test/Habits/GetAll/GetAllHabitsTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.Text.Json;
+using FluentAssertions;
+
+namespace WebApi.Test.Habits.GetAll
+{
+    public class GetAllHabitsTest : HabitsClassFixture, IClassFixture<CustomWebApplicationFactory>
+    {
+        private const string METHOD = "api/habits";
+        private readonly string _token;
+
+        public GetAllHabitsTest(CustomWebApplicationFactory webApplicationFactory) : base(webApplicationFactory)
+        {
+            _token = webApplicationFactory.TestUserToken ?? throw new InvalidOperationException("Token não gerado");
+        }
+
+        [Fact]
+        public async Task Success()
+        {
+            var result = await DoGet(requestUri: METHOD, token: _token);
+
+            result.StatusCode.Should().Be(HttpStatusCode.OK);
+
+            var body = await result.Content.ReadAsStreamAsync();
+
+            var response = await JsonDocument.ParseAsync(body);
+
+            response.RootElement.GetProperty("habits").EnumerateArray().Should().NotBeNullOrEmpty();
+        }
+    }
+}
