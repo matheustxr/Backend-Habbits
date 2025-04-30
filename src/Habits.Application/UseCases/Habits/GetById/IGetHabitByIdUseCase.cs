8c80c7f59e5773001e71f312c849e43c589dff57
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-04-30 08:56:49 -0300

    created IGetHabitByIdUseCase

---
+++ src/Habits.Application/UseCases/Habits/GetById/IGetHabitByIdUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Habits.Communication.Responses.Habits;
+
+namespace Habits.Application.UseCases.Habits.GetById
+{
+    public interface IGetHabitByIdUseCase
+    {
+        Task<ResponseHabitJson> Execute(long id);
+    }
+}
