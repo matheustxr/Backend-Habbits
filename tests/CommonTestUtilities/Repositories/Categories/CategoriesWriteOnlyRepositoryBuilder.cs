fd0f81e697d7996736c130345c088596e29ec9db
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-05-26 18:37:01 -0300

    created CategoriesWriteOnlyRepositoryBuilder

---
+++ tests/CommonTestUtilities/Repositories/Categories/CategoriesWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Habits.Domain.Repositories.Categories;
+using Moq;
+
+namespace CommonTestUtilities.Repositories.Categories
+{
+    public class CategoriesWriteOnlyRepositoryBuilder
+    {
+        private readonly Mock<ICategoriesWriteOnlyRepository> _repository;
+
+        public CategoriesWriteOnlyRepositoryBuilder()
+        {
+            _repository = new Mock<ICategoriesWriteOnlyRepository>();
+        }
+
+        public ICategoriesWriteOnlyRepository Build() => _repository.Object;
+    }
+}
