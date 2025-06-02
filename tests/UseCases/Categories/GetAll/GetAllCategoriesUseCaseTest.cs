d2458682b35306f0d359afd37b30b2135fbc1268
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-06-02 19:58:04 -0300

    feat: created test GetAllCategoriesUseCaseTest

---
+++ tests/UseCases/Categories/GetAll/GetAllCategoriesUseCaseTest.cs
@@ -0,0 +1,44 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories.Categories;
+using FluentAssertions;
+using Habits.Application.UseCases.Categories.GetAll;
+using Habits.Domain.Entities;
+
+namespace UseCases.Test.Categories.GetAll
+{
+    public class GetAllCategoriesUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var loggedUser = UserBuilder.Build();
+
+            var categories = CategoryBuilder.Collection(loggedUser);
+
+            var useCase = CreateUseCase(loggedUser, categories);
+
+            var result = await useCase.Execute();
+
+            result.Should().NotBeNull();
+            result.Categories.Should().NotBeNullOrEmpty().And.AllSatisfy(category =>
+            {
+                category.Id.Should().BeGreaterThan(0);
+                category.Category.Should().NotBeNullOrEmpty();
+                category.HexColor.Should().NotBeNullOrEmpty();
+            });
+        }
+
+        private GetAllCategoriesUseCase CreateUseCase(User user, List<HabitCategory> categories)
+        {
+            var repository = new CategoriesReadOnlyRepositoryBuilder().GetAll(user, categories).Build();
+
+            var mapper = MapperBuilder.Build();
+
+            var loggedUser = LoggedUserBuilder.Build(user);
+
+            return new GetAllCategoriesUseCase(loggedUser, repository, mapper);
+        }
+    }
+}
