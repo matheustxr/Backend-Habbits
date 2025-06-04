8a84acfc683161b98de298dfa002372565a3d8e4
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-06-04 17:49:20 -0300

    feat: add IUpdateCategoryUseCase

---
+++ src/Habits.Application/UseCases/Categories/Update/IUpdateCategoryUseCase.cs
@@ -0,0 +1,9 @@
+﻿using Habits.Communication.Requests.Categories;
+
+namespace Habits.Application.UseCases.Categories.Update
+{
+    public interface IUpdateCategoryUseCase
+    {
+        Task Execute(RequestCategoryJson request, long id);
+    }
+}
