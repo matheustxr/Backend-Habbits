f40d1f757e1df9da0a0a725e462f983b62fcc4d5
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-07-15 09:08:35 -0300

    feat: add cors configuration

--- src/Habits.Api/Program.cs
+++ src/Habits.Api/Program.cs
@@ -110,6 +110,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+app.UseCors(myAllowSpecificOrigins);
+
 app.MapHealthChecks("/Health", new HealthCheckOptions
 {
     AllowCachingResponses = false,
