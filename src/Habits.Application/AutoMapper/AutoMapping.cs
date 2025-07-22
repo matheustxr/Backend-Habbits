f34656815175458f36cd8941c1f7fec7e0b820eb
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-07-22 08:58:24 -0300

    feat: add auto mapping for responses summary

--- src/Habits.Application/AutoMapper/AutoMapping.cs
+++ src/Habits.Application/AutoMapper/AutoMapping.cs
@@ -45,5 +45,6 @@ public class AutoMapping : Profile
         CreateMap<HabitCategory, ResponseCategoryJson>();
 
         CreateMap<DayHabit, ResponseSummaryJson>();
+        CreateMap<DayHabit, ResponseSummaryHabitJson>();
     }
 }
