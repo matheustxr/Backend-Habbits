1c1c85bc138831d2b5e1d63f0bdb2ff85e9bb370
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-09-25 14:33:27 -0300

    feat: add tests for category in create and update habit

--- tests/UseCases/Habits/Create/CreateHabitUseCaseTest.cs
+++ tests/UseCases/Habits/Create/CreateHabitUseCaseTest.cs
@@ -2,11 +2,14 @@
 using CommonTestUtilities.LoggedUser;
 using CommonTestUtilities.Mapper;
 using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Repositories.Categories;
 using CommonTestUtilities.Repositories.Habits;
 using CommonTestUtilities.Requests.Habits;
 using FluentAssertions;
 using Habits.Application.UseCases.Habits.Create;
+using Habits.Application.UseCases.Habits.Update;
 using Habits.Domain.Entities;
+using Habits.Domain.Repositories.Habits;
 using Habits.Exception;
 using Habits.Exception.ExceptionBase;
 
@@ -47,10 +50,51 @@ public class CreateHabitUseCaseTest
         result.Which.GetErrors().Should().ContainSingle().And.Contain(ResourceErrorMessages.TITLE_EMPTY);
     }
 
+    [Fact]
+    public async Task Error_Category_Not_Found()
+    {
+        var loggedUser = UserBuilder.Build();
+
+        var habit = HabitBuilder.Build(loggedUser);
+
+        var request = RequestHabitJsonBuilder.Build();
+        request.UserId = loggedUser.Id;
+        request.CategoryId = 999;
+
+        var habitsReadOnlyRepository = new HabitsReadOnlyRepositoryBuilder();
+        habitsReadOnlyRepository.GetById(loggedUser, habit);
+
+        var habitsWriteOnlyRepository = new HabitsWriteOnlyRepositoryBuilder();
+
+        var categoryReadOnlyRepository = new CategoriesReadOnlyRepositoryBuilder();
+
+        var mapper = MapperBuilder.Build();
+        var unitOfWork = UnityOfWorkBuilder.Build();
+        var loggedUserService = LoggedUserBuilder.Build(loggedUser);
+
+        var useCase = new CreateHabitUseCase(
+            mapper,
+            habitsReadOnlyRepository.Build(),
+            habitsWriteOnlyRepository.Build(),
+            categoryReadOnlyRepository.Build(),
+            loggedUserService,
+            unitOfWork
+        );
+
+        var act = async () => await useCase.Execute(request);
+
+        var result = await act.Should().ThrowAsync<ErrorOnValidationException>();
+
+        result.Which.GetErrors().Should()
+            .ContainSingle()
+            .And.Contain(ResourceErrorMessages.CATEGORY_NOT_FOUND);
+    }
+
     private CreateHabitUseCase CreateUseCase(User user)
     {
-        var readOnlyRepository = new HabitsReadOnlyRepositoryBuilder().Build();
-        var writeOnlyRepository = new HabitsWriteOnlyRepositoryBuilder().Build();
+        var habitsReadOnlyRepository = new HabitsReadOnlyRepositoryBuilder().Build();
+        var habitsWriteOnlyRepository = new HabitsWriteOnlyRepositoryBuilder().Build();
+        var categoryReadOnlyRepository = new CategoriesReadOnlyRepositoryBuilder().Build();
 
         var mapper = MapperBuilder.Build();
 
@@ -58,6 +102,6 @@ public class CreateHabitUseCaseTest
 
         var loggedUser = LoggedUserBuilder.Build(user);
 
-        return new CreateHabitUseCase(mapper, readOnlyRepository, writeOnlyRepository, loggedUser, unitOfWork);
+        return new CreateHabitUseCase(mapper, habitsReadOnlyRepository, habitsWriteOnlyRepository, categoryReadOnlyRepository, loggedUser, unitOfWork);
     }
 }
